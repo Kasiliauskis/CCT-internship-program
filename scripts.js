f23f5992a90be54b8f1bb9a0eee057a257124276
Kasiliauskis <mantaskasiliauskis@gmail.com>
2021-11-09 18:26:37 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,4 @@
-
-// function myFunction() {
-//   document.querySelectorAll('.statusLine200').style.width = "300px";
-// console.log("buvo paspaustas ");
-
-// }
-
+// 1 Build test task
 function build() {
   document.getElementById("buildTask").style.width = "300px";
   document.getElementById("submitTask").style.width = "200px";
@@ -13,26 +7,29 @@ function build() {
   console.log("buvo paspaustas 1");
 }
 
+// 2 Submit your test task
 function submit() {
-    document.getElementById("buildTask").style.width = "200px";
-    document.getElementById("submitTask").style.width = "300px";
-    document.getElementById("participate").style.width = "200px";
-    document.getElementById("receive").style.width = "200px";
+  document.getElementById("buildTask").style.width = "200px";
+  document.getElementById("submitTask").style.width = "300px";
+  document.getElementById("participate").style.width = "200px";
+  document.getElementById("receive").style.width = "200px";
   console.log("buvo paspaustas 2");
 }
 
+// 3 Participate in tech interview
 function participate() {
-    document.getElementById("buildTask").style.width = "200px";
-    document.getElementById("submitTask").style.width = "200px";
-    document.getElementById("participate").style.width = "300px";
-    document.getElementById("receive").style.width = "200px";
+  document.getElementById("buildTask").style.width = "200px";
+  document.getElementById("submitTask").style.width = "200px";
+  document.getElementById("participate").style.width = "300px";
+  document.getElementById("receive").style.width = "200px";
   console.log("buvo paspaustas 3");
 }
 
+// 4 Receive answer
 function receive() {
-    document.getElementById("buildTask").style.width = "200px";
-    document.getElementById("submitTask").style.width = "200px";
-    document.getElementById("participate").style.width = "200px";
-    document.getElementById("receive").style.width = "300px";
+  document.getElementById("buildTask").style.width = "200px";
+  document.getElementById("submitTask").style.width = "200px";
+  document.getElementById("participate").style.width = "200px";
+  document.getElementById("receive").style.width = "300px";
   console.log("buvo paspaustas 4");
 }
