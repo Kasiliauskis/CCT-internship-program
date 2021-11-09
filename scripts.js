bb3d4450b14306584331edce51739ec34c4e00d2
Kasiliauskis <mantaskasiliauskis@gmail.com>
2021-11-09 18:24:08 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,38 @@
+
 // function myFunction() {
-//   document.querySelectorAll('.statusLine200').style.Width = "300px";
+//   document.querySelectorAll('.statusLine200').style.width = "300px";
+// console.log("buvo paspaustas ");
+
 // }
 
-// function kazkas() {
-//     document.getElementById("myDIV").style.maxWidth = "300px";
-//     console.log('buvo paspaustas');
-// }
+function build() {
+  document.getElementById("buildTask").style.width = "300px";
+  document.getElementById("submitTask").style.width = "200px";
+  document.getElementById("participate").style.width = "200px";
+  document.getElementById("receive").style.width = "200px";
+  console.log("buvo paspaustas 1");
+}
+
+function submit() {
+    document.getElementById("buildTask").style.width = "200px";
+    document.getElementById("submitTask").style.width = "300px";
+    document.getElementById("participate").style.width = "200px";
+    document.getElementById("receive").style.width = "200px";
+  console.log("buvo paspaustas 2");
+}
+
+function participate() {
+    document.getElementById("buildTask").style.width = "200px";
+    document.getElementById("submitTask").style.width = "200px";
+    document.getElementById("participate").style.width = "300px";
+    document.getElementById("receive").style.width = "200px";
+  console.log("buvo paspaustas 3");
+}
+
+function receive() {
+    document.getElementById("buildTask").style.width = "200px";
+    document.getElementById("submitTask").style.width = "200px";
+    document.getElementById("participate").style.width = "200px";
+    document.getElementById("receive").style.width = "300px";
+  console.log("buvo paspaustas 4");
+}
